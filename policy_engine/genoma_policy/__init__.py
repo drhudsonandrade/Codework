1d0da1b2bec015412c50198f83fe70340afbaa71
drhudsonandrade <116986656+drhudsonandrade@users.noreply.github.com>
2026-08-22 17:28:39 -0300

    fix: identify policy engine as v3.4

--- policy_engine/genoma_policy/__init__.py
+++ policy_engine/genoma_policy/__init__.py
@@ -1,7 +1,7 @@
-"""GENOMA v3.3 executable policy engine."""
+"""GENOMA v3.4 executable policy engine."""
 
 from .engine import PolicyEngine, evaluate_manifest
 from .ruleset import Ruleset, RulesetError, load_ruleset
 
 __all__ = ["PolicyEngine", "Ruleset", "RulesetError", "evaluate_manifest", "load_ruleset"]
-__version__ = "0.3.0"
+__version__ = "0.4.0"
