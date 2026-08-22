915f80b0f3e47d34d20b19cba02770c559c027a9
drhudsonandrade <116986656+drhudsonandrade@users.noreply.github.com>
2026-08-22 20:09:03 -0300

    Sort policy package exports for Ruff

--- policy_engine/genoma_policy/__init__.py
+++ policy_engine/genoma_policy/__init__.py
@@ -4,4 +4,4 @@ from .engine import PolicyEngine, evaluate_manifest
 from .ruleset import Ruleset, RulesetError, load_ruleset
 from .version import __version__
 
-__all__ = ["PolicyEngine", "Ruleset", "RulesetError", "evaluate_manifest", "load_ruleset", "__version__"]
+__all__ = ["PolicyEngine", "Ruleset", "RulesetError", "__version__", "evaluate_manifest", "load_ruleset"]
